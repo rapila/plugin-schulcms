05c960cf42e89fef166b13f385372bcc422c5398
Jürg Messmer <jmessmer@bluewin.ch>
2011-08-18 15:45:55 +0200

    new teachers_require_classes config added

--- lib/model/FunctionGroupPeer.php
+++ lib/model/FunctionGroupPeer.php
@@ -8,11 +8,15 @@
 class FunctionGroupPeer extends BaseFunctionGroupPeer {
 
 	public static function getFunctionGroupIdsForTeamlist() {
-		return array_merge(self::getFunctionGroupIdsForTeachers(), self::getFunctionGroupIdsForOthers());
+		return array_merge(self::getFunctionGroupIdsForTeachers(), self::getFunctionGroupIdsForTeachersRequireClasses(), self::getFunctionGroupIdsForOthers());
 	}
 	
 	public static function getFunctionGroupIdsForTeachers() {
 		return SchoolPeer::getActiveFunctionGroupIds('teachers');
+	}
+
+	public static function getFunctionGroupIdsForTeachersRequireClasses() {
+		return SchoolPeer::getActiveFunctionGroupIds('teachers_require_classes');
 	}
 	
 	public static function getFunctionGroupIdsForOthers() {
