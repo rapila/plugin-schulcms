b5254f489360238896e1daa93464cc3ecae117ef
Jürg Messmer <jmessmer@bluewin.ch>
2011-08-21 11:50:33 +0200

    text turned to string

--- modules/frontend/classes/ClassesFrontendModule.php
+++ modules/frontend/classes/ClassesFrontendModule.php
@@ -75,7 +75,7 @@ class ClassesFrontendModule extends DynamicFrontendModule implements WidgetBased
 				}
 			}
 			if(count($aClassTeachers) > $iLimit) {
-				$oItemTemplate->replaceIdentifierMultiple('class_teacher_links', ', usw.');
+				$oItemTemplate->replaceIdentifierMultiple('class_teacher_links', ', '.StringPeer::getString('wns.word.etc'));
 			}
 			$oTemplate->replaceIdentifierMultiple('list_item', $oItemTemplate);
 		}
