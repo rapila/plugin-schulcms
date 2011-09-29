1edc6fd441d6fb08b153d54e40d2ed8f3fbaaa17
Jürg Messmer <jm@mosaics.ch>
2011-09-29 16:33:44 +0200

    fixed: display virtual also if there are no current events
    refactored EventFilterModule so its guaranteed that year virtual navigation items are always created

--- lib/model/EventPeer.php
+++ lib/model/EventPeer.php
@@ -2,14 +2,14 @@
 
 
 /**
- * @package    propel.generator.model
+ * @package		 propel.generator.model
  */
 class EventPeer extends BaseEventPeer {
 	
 	const EVENT_TYPE_DEFAULT = 1;
 	const EVENT_TYPE_PROJECT = 2;
 
-  public static function resetShowOnFrontpage() {
+	public static function resetShowOnFrontpage() {
 		$oCriteria = new Criteria();
 		$oCriteria->add(self::SHOW_ON_FRONTPAGE, true);
 		foreach(self::doSelect($oCriteria) as $oResetEvent) {
@@ -22,5 +22,21 @@ class EventPeer extends BaseEventPeer {
 		$oSearchCriterion = $oCriteria->getNewCriterion(self::TITLE, "%$sSearch%", Criteria::LIKE);
 		$oSearchCriterion->addOr($oCriteria->getNewCriterion(self::TEASER, "%$sSearch%", Criteria::LIKE));
 		$oCriteria->add($oSearchCriterion);
+	}
+	
+	public static function getYears($iEventType = null) {
+		$oCriteria = new Criteria();
+		$oCriteria->clearSelectColumns()->clearOrderByColumns()->clearGroupByColumns();
+		$oCriteria->setDistinct();
+		if(is_numeric($iEventType)) {
+			$oCriteria->add(self::EVENT_TYPE_ID, $iEventType);
+		}
+		$oCriteria->addSelectColumn('YEAR(' . self::DATE_START . ') AS Year');
+		$oCriteria->addAscendingOrderByColumn('year');
+		$aYears = array();
+		foreach(self::doSelectStmt($oCriteria)->fetchAll(PDO::FETCH_CLASS) as $aParams) {
+			$aYears[] = $aParams->Year;
+		}
+		return $aYears;
 	}
 }
