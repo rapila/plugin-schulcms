469ebc830651d3122941f0623fa4482d08d7c59d
Jürg Messmer <jm@mosaics.ch>
2011-10-26 12:54:55 +0200

    permission handling added for class_teachers

--- lib/model/SchoolClassPeer.php
+++ lib/model/SchoolClassPeer.php
@@ -62,5 +62,26 @@ class SchoolClassPeer extends BaseSchoolClassPeer {
 		return $oCriteria;
 	}
 	
+	public static function mayOperateOn($oUser, $mObject, $sOperation) {
+		// allow all users with module rights
+		if(parent::mayOperateOn($oUser, $mObject, $sOperation)) {
+			return true;
+		}
+		if($oUser === null) {
+			return false;
+		}
+		// allow all users that are team members and class teachers to handle their own events
+		$aTeamMembers = $oUser->getTeamMembersRelatedByUserId();
+		if(isset($aTeamMembers[0])) {
+			foreach($aTeamMembers[0]->getClassTeachersJoinSchoolClassesForPermissions(true) as $oClassTeacher) {
+				if($oClassTeacher->getSchoolClass()->getId() === $mObject->getId()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	
 }
 
