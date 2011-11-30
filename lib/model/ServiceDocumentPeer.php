8a8478ffb67cd5c8d9c49c45d2cf845f6f6a91ca
Jürg Messmer <jm@mosaics.ch>
2011-11-30 17:23:21 +0100

    refactored to use query class, cleanup

--- lib/model/ServiceDocumentPeer.php
+++ lib/model/ServiceDocumentPeer.php
@@ -4,11 +4,6 @@
  * @package    propel.generator.model
  */
 class ServiceDocumentPeer extends BaseServiceDocumentPeer {
-	
-	public static function countDocumentsByServiceId($iServiceId) {
-		$oCriteria = new Criteria();
-		$oCriteria->add(self::SERVICE_ID, $iServiceId);
-		return self::doCount($oCriteria);
-	}
+
 }
 
--- lib/model/ServiceQuery.php
+++ lib/model/ServiceQuery.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 /**
  * @package    propel.generator.model
  */
