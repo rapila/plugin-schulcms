7f22673c5d7ebdc8ea2c765199441a415e0a7c4a
Jürg Messmer <jm@mosaics.ch>
2011-12-18 17:07:21 +0100

    my_services widget added
    argument removed in js file

---
+++ modules/widget/my_services/MyServicesWidgetModule.php
@@ -0,0 +1,36 @@
+<?php
+
+class MyServicesWidgetModule extends PersistentWidgetModule {
+	
+	private $oTeamMember;
+	
+	public function __construct() {
+		$oUser = Session::getSession()->getUser();
+		if($oUser) {
+			$this->oTeamMember = TeamMemberQuery::create()->filterByUserId($oUser->getId())->findOne();
+		}
+	}
+	
+	public function listServices($aSettings) {
+		$aResult = array();
+		if(!$this->oTeamMember) {
+			return $aResult;
+		}
+		$oQuery = ServiceMemberQuery::create()->joinService()->useServiceQuery()->orderByName()->endUse()->filterByTeamMemberId($this->oTeamMember->getId());
+		$oServicesPage = PageQuery::create()->filterByIdentifier(SchoolPeer::PAGE_IDENTIFIER_SERVICES)->findOne();
+		
+		foreach($oQuery->find() as $oServiceMember) {
+			$aServiceInfo = array();
+			$oService = $oServiceMember->getService();
+			$aServiceInfo['Id'] = $oServiceMember->getServiceId();			
+			$aServiceInfo['Name'] = $oService->getName();
+			$aServiceInfo['CountDocuments']	= $oService->countServiceDocuments();
+			$aServiceInfo['CountMembers']	= $oService->countServiceMembers();
+			$aServiceInfo['HasLogo'] = $oService->getLogoId() != null;
+			$aServiceInfo['Category']	= $oService->getServiceCategory()->getName();
+			$aServiceInfo['ServiceLink'] = LinkUtil::link($oService->getServiceLink($oServicesPage), 'FrontendManager');
+			$aResult[] = $aServiceInfo;
+		}
+		return $aResult;
+	}
+}
