d6b3f4ef24e5f9852fe1e4230015d0ccdf6bac48
Jürg Messmer <jm@mosaics.ch>
2012-01-11 11:03:34 +0100

    various little css details, truncate added to link text in my_classes

--- modules/frontend/notes/NotesFrontendModule.php
+++ modules/frontend/notes/NotesFrontendModule.php
@@ -15,41 +15,57 @@ class NotesFrontendModule extends DynamicFrontendModule {
 			return null;
 		}
 		switch($aOptions[self::MODE_SELECT_KEY]) {
-			case 'current_note': return $this->renderCurrentNote(@$aOptions['note_type_id']);
+			case 'current_note': return $this->renderCurrentNote(@$aOptions['note_type_id'], @$aOptions['limit']);
 			default:
 				return null;
 		}
 	}
 
-	public function renderCurrentNote($iNoteTypeId = null) {
+	public function renderCurrentNote($iNoteTypeId=null, $iLimit=null) {
 		$oQuery = NoteQuery::create()->filterByIsInactive(false);
 		if($iNoteTypeId !== null) {
 			$oQuery->filterByNoteTypeId($iNoteTypeId);
 		}
 		$oQuery->filterByDate()->orderByDateStart();
-		$oNote = $oQuery->findOne();
-		if($oNote && is_resource($oNote->getBody())) {
-			$oTemplate = $this->constructTemplate('note');
-			if($oNoteType = $oNote->getNoteType()){
-				$oTemplate->replaceIdentifier('note_type_name', $oNoteType->getName());
+		if($iLimit) {
+			$oQuery->limit($iLimit);
+		}
+
+		foreach($oQuery->find() as $oNote) {
+			if($oNote && is_resource($oNote->getBody())) {
+				$oTemplate = $this->constructTemplate('note');
+				if($oNoteType = $oNote->getNoteType()){
+					$oTemplate->replaceIdentifier('note_type_name', $oNoteType->getName());
+				}
+				$sContent = stream_get_contents($oNote->getBody());
+				if($sContent != '') {
+					$oTemplate->replaceIdentifier('content', RichtextUtil::parseStorageForFrontendOutput($sContent));
+				}
+				return $oTemplate;
 			}
-			$sContent = stream_get_contents($oNote->getBody());
-			if($sContent != '') {
-				$oTemplate->replaceIdentifier('content', RichtextUtil::parseStorageForFrontendOutput($sContent));
-			}
-			return $oTemplate;
 		}
 		return null;
 	}
 	
 	public function renderBackend() {
 		$oTemplate = $this->constructTemplate('config');
-		$aOptions = array();
+
+		// display option
+		$aDisplayOptions = array();
 		foreach(self::$DISPLAY_MODES as $sDisplayMode) {
-			$aOptions[$sDisplayMode] = StringPeer::getString('notes.display_option.'.$sDisplayMode, null, StringUtil::makeReadableName($sDisplayMode));
+			$aDisplayOptions[$sDisplayMode] = StringPeer::getString('notes.display_option.'.$sDisplayMode, null, StringUtil::makeReadableName($sDisplayMode));
 		}
-		$oTemplate->replaceIdentifier('display_options', TagWriter::optionsFromArray($aOptions, null, null, null));
+		$oTemplate->replaceIdentifier('display_options', TagWriter::optionsFromArray($aDisplayOptions, null, null, null));
+
+		// note_type option
 		$oTemplate->replaceIdentifier('type_options', TagWriter::optionsFromObjects(NoteTypeQuery::create()->orderByName()->find(), 'getId', 'getName', null, array('' => StringPeer::getString('wns.notes.all_note_types'))));
+		
+		// display limit
+		$aLimitOptions = array();
+		foreach(range(1,3) as $iLimit) {
+			$aLimitOptions[$iLimit] = $iLimit;
+		}
+		$oTemplate->replaceIdentifier('limit_options', TagWriter::optionsFromArray($aLimitOptions, null, null, array('' => StringPeer::getString('wns.note.limit_none'))));
 		return $oTemplate;
 	}
 }
