7e9b177755492342c46420995c368818dd617f49
Jürg Messmer <jm@mosaics.ch>
2012-06-25 17:57:39 +0200

    missing text added and handling improved, cleanup

--- modules/widget/events_frontend_config/EventsFrontendConfigWidgetModule.php
+++ modules/widget/events_frontend_config/EventsFrontendConfigWidgetModule.php
@@ -2,22 +2,28 @@
 class EventsFrontendConfigWidgetModule extends FrontendConfigWidgetModule { 
 
 	public function getDisplayModes() {
-	  $aResult = array();
-	  $aEventTypes = EventTypeQuery::create()->orderByName()->find();
+		$aResult = array();
+		
+		// Display modes
+		$aResult['display_modes'] = array();
+		foreach(EventsFrontendModule::$DISPLAY_MODES as $sDisplayMode) {
+			$aResult['display_modes'][$sDisplayMode] = StringPeer::getString('display_mode.'.$sDisplayMode, null, $sDisplayMode);
+		}
+	
+		// Event types
+		$aEventTypes = EventTypeQuery::create()->orderByName()->find();
 		if(count($aEventTypes) > 0) {
 			$aResult['event_types'] = array();
 			foreach($aEventTypes as $oEventType) {
 				$aResult['event_types'][$oEventType->getId()] = $oEventType->getName();
 			}
 		}
-		$aResult['display_modes'] = array();
-	  foreach(EventsFrontendModule::$DISPLAY_MODES as $sDisplayMode) {
-	    $aResult['display_modes'][$sDisplayMode] = StringPeer::getString('display_mode.'.$sDisplayMode, null, $sDisplayMode);
-	  }
+		
+		// Display list limit
 		$aResult['event_limits'] = array('' => StringPeer::getString('wns.events.display_all'));
-	  foreach(range(1,4) as $iCount) {
-	    $aResult['event_limits'][$iCount] = $iCount;
-	  }
+		foreach(range(1,4) as $iCount) {
+			$aResult['event_limits'][$iCount] = $iCount;
+		}
 		return $aResult;
 	}
 	
