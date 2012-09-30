9f09647856c9df8f07a53e8ee84256a03fe061a1
Jürg Messmer <jm@mosaics.ch>
2012-09-30 10:57:19 +0200

    removed unused methods
    replaced inappropriate static parent call by $this

--- lib/model/ClassType.php
+++ lib/model/ClassType.php
@@ -14,18 +14,10 @@ class ClassType extends BaseClassType {
 		return $this->getOriginalName();
 	}
 	
-	public function getClassCountCurrentYear() {
-		return $this->getClassCount();
-	}
-
-	public function getClassCountOtherYears() {
-		return $this->getClassCount(false);
-	}
-	
 	private function getClassCount($bCurrentYear=true) {
 		$oCriteria = new Criteria();
 		$oCriteria->add(SchoolClassPeer::YEAR, SchoolPeer::getCurrentYear(), $bCurrentYear ? Criteria::EQUAL : Criteria::NOT_EQUAL);
-		return parent::countSchoolClasss($oCriteria);
+		return $this->countSchoolClasss($oCriteria);
 	}
 
 }
