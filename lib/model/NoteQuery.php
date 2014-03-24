c1997a6ee2e51225138307a59a759166944164a9
Jürg Messmer <jm@mosaics.ch>
2014-03-24 16:47:58 +0100

    missing methods added

--- lib/model/NoteQuery.php
+++ lib/model/NoteQuery.php
@@ -10,5 +10,13 @@ class NoteQuery extends BaseNoteQuery {
 		$this->filterByDateStart($sDateToday, Criteria::LESS_EQUAL)->_and()->filterByDateEnd($sDateToday, Criteria::GREATER_EQUAL)->_or()->filterByDateEnd(null, Criteria::ISNULL);
 		return $this;
 	}
+	
+	public function publishedOnly() {
+		return $this->filterByIsInactive(false);
+	}
+	
+	public function orderByDate() {
+		return $this->orderByDateStart();
+	}
 }
 
