c3adc65845b2099e6b629cfe9e0fe1037d3a86e3
Jürg Messmer <jm@mosaics.ch>
2014-04-26 13:43:15 +0200

    set initial_selection if there is only one type
    • since a event_type is mandatory, a inital type can be set, if there is no choice anyway

--- modules/widget/event_types_input/EventTypesInputWidgetModule.php
+++ modules/widget/event_types_input/EventTypesInputWidgetModule.php
@@ -3,16 +3,24 @@
 * @package modules.widget
 */
 class EventTypesInputWidgetModule extends WidgetModule {
-		
-  public function allEventTypes() {
-		$aEventTypes = EventTypeQuery::create()->orderById()->find();
+
+	private $aEventTypes = array();
+	
+	public function __construct() {
+		$this->aEventTypes = EventTypeQuery::create()->orderById()->find();
+		if(count($this->aEventTypes) === 1) {
+			$this->setSetting('initial_selection', (string) $this->aEventTypes[0]->getId());
+		}
+	}
+
+	public function allEventTypes() {
 		$aResult = array();
-		foreach($aEventTypes as $oEventType) {
-      $aResult[$oEventType->getId()] = $oEventType->getName();
+		foreach($this->aEventTypes as $oEventType) {
+			$aResult[$oEventType->getId()] = $oEventType->getName();
 		}
 		return $aResult;
 	}
-	
+
 	public function getElementType() {
 		return 'select';
 	}
