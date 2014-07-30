b44df0f3768e8d5aeac4462c566665664b4c923a
Jürg Messmer <jm@mosaics.ch>
2014-07-30 11:21:17 +0200

    make column display_type_numeric for aligning

--- modules/widget/class_list/ClassListWidgetModule.php
+++ modules/widget/class_list/ClassListWidgetModule.php
@@ -9,7 +9,7 @@ class ClassListWidgetModule extends WidgetModule {
 	public $oClassesWithStudentsInputFilter;
 	public $oSchoolYearInputFilter;
 	public $bShowWithStudentsOnly;
-	
+
 	public function __construct() {
 		$this->oListWidget = new ListWidgetModule();
 		$this->oDelegateProxy = new CriteriaListWidgetDelegate($this, "SchoolClass", "name", "asc");
@@ -19,18 +19,18 @@ class ClassListWidgetModule extends WidgetModule {
 		$this->oDelegateProxy->setYearPeriod(SchoolPeer::getCurrentYear());
 		$this->oSchoolYearInputFilter = WidgetModule::getWidget('year_input', null, $this->oDelegateProxy->getYearPeriod());
 	}
-	
+
 	public function doWidget() {
 		$aTagAttributes = array('class' => 'class_list');
 		$oListTag = new TagWriter('table', $aTagAttributes);
 		$this->oListWidget->setListTag($oListTag);
 		return $this->oListWidget->doWidget();
 	}
-	
+
 	public function getColumnIdentifiers() {
 		return array('id', 'name', 'teaching_unit_name', 'class_type_id', 'year_period', 'level', 'count_students', 'class_teacher_names', 'has_class_portrait', 'has_class_schedule', 'count_events');
 	}
-	
+
 	public function getMetadataForColumn($sColumnIdentifier) {
 		$aResult = array('is_sortable' => true);
 		switch($sColumnIdentifier) {
@@ -51,12 +51,13 @@ class ClassListWidgetModule extends WidgetModule {
 				break;
 			case 'level':
 				$aResult['heading'] = StringPeer::getString('wns.class.level');
+				$aResult['display_type'] = ListWidgetModule::DISPLAY_TYPE_NUMERIC;
 				break;
 			case 'count_students':
 				$aResult['heading'] = StringPeer::getString('wns.class.with_students');
 				$aResult['heading_filter'] = array('classes_with_students_input', $this->oClassesWithStudentsInputFilter->getSessionKey());
 				$aResult['is_sortable'] = false;
-				break;			
+				break;
 			case 'class_teacher_names':
 				$aResult['heading'] = StringPeer::getString('wns.class.class_teacher');
 				$aResult['is_sortable'] = false;
@@ -75,11 +76,11 @@ class ClassListWidgetModule extends WidgetModule {
 			case 'count_events':
 				$aResult['heading'] = StringPeer::getString('wns.class.events');
 				$aResult['is_sortable'] = false;
-				break;				
+				break;
 			case 'count_links':
 				$aResult['heading'] = StringPeer::getString('wns.class.links');
 				$aResult['is_sortable'] = false;
-				break;			
+				break;
 		}
 		return $aResult;
 	}
@@ -93,19 +94,19 @@ class ClassListWidgetModule extends WidgetModule {
 		}
 		if($sColumnIdentifier === 'has_class_portrait') {
 			return SchoolClassPeer::CLASS_PORTRAIT_ID;
-		}		
+		}
 		if($sColumnIdentifier === 'teaching_unit_name') {
 			return SchoolClassPeer::TEACHING_UNIT;
-		}		
+		}
 		if($sColumnIdentifier === 'has_class_schedule') {
 			return SchoolClassPeer::CLASS_SCHEDULE_ID;
-		}		
+		}
 		if($sColumnIdentifier === 'class_teacher_name') {
 			return TeamMemberPeer::LAST_NAME;
 		}
 		return null;
 	}
-	
+
 	public function getFilterTypeForColumn($sColumnIdentifier) {
 		if($sColumnIdentifier === 'class_type_id') {
 			return CriteriaListWidgetDelegate::FILTER_TYPE_IS;
@@ -115,11 +116,11 @@ class ClassListWidgetModule extends WidgetModule {
 		}
 		return null;
 	}
-	
+
 	public function setCountStudents($bShowWithStudentsOnly) {
 		$this->bShowWithStudentsOnly = $bShowWithStudentsOnly;
 	}
-	
+
 	public function getClassTypeName() {
 		if($this->oDelegateProxy->getClassTypeId() === CriteriaListWidgetDelegate::SELECT_ALL) {
 			return null;
