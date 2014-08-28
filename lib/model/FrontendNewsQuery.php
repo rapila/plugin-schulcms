28275efc2042c09a307bd3819d45b9f4a29de9dc
Jürg Messmer <jm@mosaics.ch>
2014-08-28 08:22:11 +0200

    add FrontendNewsQuery class for simplicity...
    • add method current() that gets only entries with correct start and end dates

---
+++ lib/model/FrontendNewsQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package model
+ * @subpackage rapila-plugin-journal
+ */
+class FrontendNewsQuery extends NewsQuery {
+
+	public static function create($sModelAlias = null, $oCriteria = null) {
+		if ($oCriteria instanceof FrontendNewsQuery) {
+			return $oCriteria;
+		}
+		$oQuery = new FrontendNewsQuery();
+		if (null !== $sModelAlias) {
+			$oQuery->setModelAlias($sModelAlias);
+		}
+		if ($oCriteria instanceof Criteria) {
+			$oQuery->mergeWith($oCriteria);
+		}
+		$oQuery->filterByIsInactive(false);
+		return $oQuery;
+	}
+}
--- lib/model/NewsQuery.php
+++ lib/model/NewsQuery.php
@@ -8,4 +8,10 @@ class NewsQuery extends BaseNewsQuery
 	public function excludeExternallyManaged() {
 		return $this->useNewsTypeQuery()->filterByIsExternallyManaged(false)->_or()->filterByIsExternallyManaged(null, Criteria::ISNULL)->endUse();
 	}
+
+	public function current() {
+		$sDateToday = date('Y-m-d');
+		$this->filterByDateStart($sDateToday, Criteria::LESS_EQUAL)->_and()->filterByDateEnd($sDateToday, Criteria::GREATER_EQUAL)->_or()->filterByDateEnd(null, Criteria::ISNULL);
+		return $this;
+	}
 }
