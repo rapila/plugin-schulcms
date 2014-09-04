a5c520f9f2d115492ca066c817276782689ab62d
Jürg Messmer <jm@mosaics.ch>
2014-09-04 12:35:34 +0200

    fix and update code related to previous migrations

--- lib/model/ServicePeer.php
+++ lib/model/ServicePeer.php
@@ -7,11 +7,11 @@ class ServicePeer extends BaseServicePeer {
 
 	public static function addSearchToCriteria($sSearch, $oCriteria) {
 		$oSearchCriterion = $oCriteria->getNewCriterion(self::NAME, "%$sSearch%", Criteria::LIKE);
-		$oSearchCriterion->addOr($oCriteria->getNewCriterion(self::TEASER, "%$sSearch%", Criteria::LIKE));
+		$oSearchCriterion->addOr($oCriteria->getNewCriterion(self::BODY, "%$sSearch%", Criteria::LIKE));
 		$oSearchCriterion->addOr($oCriteria->getNewCriterion(self::ADDRESS, "%$sSearch%", Criteria::LIKE));
 		$oCriteria->add($oSearchCriterion);
 	}
-	
+
 	public static function mayOperateOn($oUser, $mObject, $sOperation) {
 		// allow all users with module rights
 		if(parent::mayOperateOn($oUser, $mObject, $sOperation)) {
