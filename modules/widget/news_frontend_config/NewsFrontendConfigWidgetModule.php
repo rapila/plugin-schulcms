6c8d64027ddc77918e48a47b416be85908dbde0c
Jürg Messmer <jm@mosaics.ch>
2014-09-10 16:29:43 +0200

    add news_frontend_config widget

---
+++ modules/widget/news_frontend_config/NewsFrontendConfigWidgetModule.php
@@ -0,0 +1,37 @@
+<?php
+class NewsFrontendConfigWidgetModule extends FrontendConfigWidgetModule {
+	public function __construct($sSessionKey, $oFrontendModule) {
+		parent::__construct($sSessionKey, $oFrontendModule);
+	}
+
+	public function options() {
+		$aData['display_options'] = $this->getDisplayOptions();
+		$aData['news_type_options'] = $this->getNewsTypeOptions();
+		$aData['limit_options'] = array(1 => 1, 2 => 3, 5 => 5, 10 => 10, "__all" => "Alle");
+		return $aData;
+	}
+
+	public function listNews($aData) {
+		$oNewsQuery = NewsQuery::create()->publishedOnly();
+		if($aData['news_type'] !== null) {
+			$oNewsQuery->filterByNewsTypeId($aData['news_type']);
+		}
+		if($aData['limit']) {
+			$oNewsQuery->limit($aData['limit']);
+		}
+		return WidgetJsonFileModule::jsonBaseObjects($oNewsQuery->orderByDate()->find(), array('id', 'headline'));
+	}
+
+	private function getDisplayOptions() {
+		$aResult = array();
+		foreach(NewssFrontendModule::$DISPLAY_MODES as $sDisplayMode) {
+			$aResult[$sDisplayMode] = StringPeer::getString('display_mode.'.$sDisplayMode, null, $sDisplayMode);
+		}
+		return $aResult;
+	}
+
+	private function getNewsTypeOptions() {
+		$aResult = array();
+		return NewsTypeQuery::create()->orderByName()->find()->toKeyValue('Id', 'Name');
+	}
+}
