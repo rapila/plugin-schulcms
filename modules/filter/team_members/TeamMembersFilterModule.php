d432860c0509220f306fd28a088003b52fe25e72
Jürg Messmer <jm@mosaics.ch>
2014-10-29 15:55:10 +0100

    add new filter and page_type for team_members

---
+++ modules/filter/team_members/TeamMembersFilterModule.php
@@ -0,0 +1,3 @@
+<?php
+class TeamMembersFilterModule extends FilterModule {
+}
---
+++ modules/page_type/team_members/TeamMembersPageTypeModule.php
@@ -0,0 +1,62 @@
+<?php
+class TeamMembersPageTypeModule extends PageTypeModule {
+	private function $aFunctionGroupIds;
+
+	public function __construct(Page $oPage = null, NavigationItem $oNavigationItem = null) {
+		parent::__construct($oPage, $oNavigationItem);
+	}
+
+	public function display(Template $oTemplate, $bIsPreview = false) {
+		if($this->oNavigationItem instanceof TeamMemberNavigationItem){
+			$this->oTeamMember = $this->oNavigationItem->getTeamMember();
+			$this->renderDetail($oTemplate);
+		}
+		$this->renderList($oTemplate);
+	}
+
+	private function renderList($oTemplate) {
+		$oListTemplate = $this->constructTemplate('list');
+		$oItemPrototype = $this->constructTemplate('list_item');
+		foreach($this->listQuery()->find() as $oTeamMember) {
+			$oItemTemplate = clone $oItemPrototype;
+			$oItemTemplate->replaceIdentifier('detail_link', LinkUtil::link($oTeamMember->getLink($this->oPage)));
+			$oItemTemplate->replaceIdentifier('name', $oTeamMember->getFullNameInverted());
+			$oItemTemplate->replaceIdentifier('detail_link_title', StringPeer::getString('wns.team_member.link_title_prefix').$oTeamMember->getFullName());
+			$oItemTemplate->replaceIdentifier('first_function_name', $oTeamMember->getFirstTeamMemberFunctionName());
+
+			$aClassTeachers = $oTeamMember->getIsClassTeacherClasses(true);
+			if(count($aClassTeachers) > 0) {
+				foreach($aClassTeachers as $i => $oClassTeacher) {
+					if($i > 0) {
+						$oItemTemplate->replaceIdentifierMultiple('school_class', ', ', null, Template::NO_NEWLINE);
+					}
+					$aLink = $oClassTeacher->getSchoolClass()->getClassLink($this->oClassPage);
+					$oItemTemplate->replaceIdentifierMultiple('school_class', TagWriter::quickTag('a', array('title' => StringPeer::getString('wns.class.link_title_prefix').$oClassTeacher->getSchoolClass()->getName(), 'href' => LinkUtil::link($aLink)), $oClassTeacher->getSchoolClass()->getName()), null, Template::NO_NEWLINE);
+				}
+			} else {
+				$oItemTemplate->replaceIdentifier('school_class', '-');
+			}
+
+			$oItemTemplate->replaceIdentifier('profession', $oTeamMember->getProfession());
+			$oTemplate->replaceIdentifierMultiple('list_item', $oItemTemplate);
+		}
+		$oTemplate->replaceIdentifier('container', $oListTemplate, 'content');
+		$oTemplate->replaceIdentifier('container_filled_types', 'faqs', 'content');
+	}
+
+	private function renderDetail($oTemplate) {
+
+	}
+
+	private function listQuery() {
+		$oQuery = TeamMemberQuery::create()->excludeInactive();
+		if($this->aFunctionGroupIds !== null) {
+			$oQuery->filterByTeamMemberFunctionGroup($this->aFunctionGroupIds);
+		}
+		$oQuery->orderByLastName()->orderByFirstName();
+		return $oQuery;
+	}
+
+
+
+}
