12d2fcf0ee8eef53d34b0a6406073c1daf9db58c
Jürg Messmer <jm@mosaics.ch>
2015-04-02 17:31:30 +0200

    add missing argument to fix default
    • fixes #4

--- modules/widget/active_team_member_input/ActiveTeamMemberInputWidgetModule.php
+++ modules/widget/active_team_member_input/ActiveTeamMemberInputWidgetModule.php
@@ -6,9 +6,9 @@ class ActiveTeamMemberInputWidgetModule extends WidgetModule {
 
 	private $bShowActiveMembersOnly;
 
-	public function __construct($bDefaultSelection = true) {
-		parent::__construct();
+	public function __construct($sSession = null, $bDefaultSelection = false) {
 		$this->bShowActiveMembersOnly = $bDefaultSelection;
+		parent::__construct($sSession);
 	}
 
 	public function getElementType() {
