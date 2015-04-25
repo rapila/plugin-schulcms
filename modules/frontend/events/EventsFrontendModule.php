956a95f572ea1fc05dd5b2132c1f2874e5837092
Jürg Messmer <jm@mosaics.ch>
2015-04-25 08:32:58 +0200

    fix truncate of html elements

--- modules/frontend/events/EventsFrontendModule.php
+++ modules/frontend/events/EventsFrontendModule.php
@@ -337,6 +337,7 @@ class EventsFrontendModule extends DynamicFrontendModule {
 			$sContent = RichtextUtil::parseStorageForFrontendOutput($sContent);
 		}
 		if($bStripTags || $iTruncate) {
+			$sContent = html_entity_decode($sContent);
 			$sContent = strip_tags($sContent);
 			if($iTruncate) {
 				$sContent = StringUtil::truncate($sContent, $iTruncate);
