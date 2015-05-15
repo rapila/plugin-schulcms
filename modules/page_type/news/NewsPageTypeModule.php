399f96b757272e22b04c831ac4b2442b2a730857
Jürg Messmer <jm@mosaics.ch>
2015-05-15 15:55:25 +0200

    refactor news page type
    • include wok filter
    • add title to list and detail because this module should use full_bleed template without title
    • add new strings

--- modules/page_type/news/NewsPageTypeModule.php
+++ modules/page_type/news/NewsPageTypeModule.php
@@ -2,6 +2,7 @@
 class NewsPageTypeModule extends PageTypeModule {
 
 	private $aNewsTypes;
+	private $bRequiresFilter = true;
 
 	public function __construct(Page $oPage = null, NavigationItem $oNavigationItem = null) {
 		parent::__construct($oPage, $oNavigationItem);
@@ -19,17 +20,46 @@ class NewsPageTypeModule extends PageTypeModule {
 	private function renderList($oTemplate) {
 		$oListTemplate = $this->constructTemplate('list');
 		$oItemPrototype = $this->constructTemplate('list_item');
+		if(!$oListTemplate->hasIdentifier('filters')) {
+			$this->bRequiresFilter = false;
+		}
 
+		$aTypes = array();
+		$oListTemplate->replaceIdentifier('title', FrontendManager::$CURRENT_NAVIGATION_ITEM->getTitle());
 		foreach($this->listQuery()->find() as $oNews) {
+			if($this->bRequiresFilter && !isset($aTypes[$oNews->getNewsTypeId()])) {
+				$aTypes[$oNews->getNewsTypeId()] = $oNews->getNewsType()->getName();
+			}
 			$oItemTemplate = clone $oItemPrototype;
 			$oItemTemplate->replaceIdentifier('detail_link', LinkUtil::link($oNews->getLink($this->oPage)));
 			$oItemTemplate->replaceIdentifier('headline', $oNews->getHeadline());
 			$oItemTemplate->replaceIdentifier('news_type', $oNews->getNewsTypeName());
+			$oItemTemplate->replaceIdentifier('type_id', $oNews->getNewsTypeId());
+			$oItemTemplate->replaceIdentifier('id', $oNews->getId());
 			$oItemTemplate->replaceIdentifier('date', $oNews->getDateStartFormatted());
 			$oListTemplate->replaceIdentifierMultiple('items', $oItemTemplate);
 		}
+		$this->renderFilter($oListTemplate, $aTypes);
 		$oTemplate->replaceIdentifier('container', $oListTemplate, 'content');
 		$oTemplate->replaceIdentifier('container_filled_types', 'news', 'content');
+	}
+
+	private function renderFilter($oTemplate, $aTypes) {
+		// Implement function group filter if it's included in the template and options are given
+		if($this->bRequiresFilter && count($aTypes) > 1) {
+			$oOptionPrototype = $this->constructTemplate('filter_option');
+			$oItemTemplate = clone $oOptionPrototype;
+			$oItemTemplate->replaceIdentifier('id', '');
+			$oItemTemplate->replaceIdentifier('name', StringPeer::getString('news.filter.type.default'));
+			$oTemplate->replaceIdentifierMultiple('filters', $oItemTemplate, null, Template::NO_NEWLINE|Template::NO_NEW_CONTEXT);
+
+			foreach($aTypes as $sKey => $sName) {
+				$oItemTemplate = clone $oOptionPrototype;
+				$oItemTemplate->replaceIdentifier('id', $sKey);
+				$oItemTemplate->replaceIdentifier('name', $sName);
+				$oTemplate->replaceIdentifierMultiple('filters', $oItemTemplate, null, Template::NO_NEWLINE|Template::NO_NEW_CONTEXT);
+			}
+		}
 	}
 
 	private function renderDetail($oTemplate) {
@@ -37,6 +67,7 @@ class NewsPageTypeModule extends PageTypeModule {
 			return null;
 		}
 		$oDetailTemplate = $this->constructTemplate('detail');
+		$oDetailTemplate->replaceIdentifier('title', FrontendManager::$CURRENT_NAVIGATION_ITEM->getTitle());
 		$oDetailTemplate->replaceIdentifier('headline', $this->oNews->getHeadline());
 		$sContent = is_resource($this->oNews->getBody()) ? stream_get_contents($this->oNews->getBody()) : '';
 		$oDetailTemplate->replaceIdentifier('content', RichtextUtil::parseStorageForFrontendOutput($sContent));
