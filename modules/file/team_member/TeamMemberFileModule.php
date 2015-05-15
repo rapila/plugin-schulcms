0909737e828896a231e56e10c6b6284e1515177e
Jürg Messmer <jm@mosaics.ch>
2015-05-15 10:08:44 +0200

    implement redirect to appropriate target page
    … depending on main function of person that is requested
    • redirect to teachers profile if is a teacher or fachperson
    • redirect to main pages 'sekreatriat', 'schulleitung', 'hauswartung', or 'ueber-uns' depending on function
    • fallback redirect to home page

--- modules/file/team_member/TeamMemberFileModule.php
+++ modules/file/team_member/TeamMemberFileModule.php
@@ -6,7 +6,42 @@ class TeamMemberFileModule extends FileModule {
 
 	public function renderFile() {
 		$oTeamMember = TeamMemberQuery::create()->filterByOriginalId(Manager::usePath())->findOne();
-		// absolute link!
-		// if($oTeamMember)
+		$oSchoolFunction = null;
+		$sFunctionGroupName = null;
+		if($oTeamMember) {
+			$oSchoolFunction = $oTeamMember->getFirstTeamMemberFunctionName(false);
+			$sFunctionGroupName = $oSchoolFunction->getFunctionGroupName();
+		}
+		// Switch target page name or identifier
+		switch($sFunctionGroupName) {
+			case('Sekretariate'): $sPageName = 'sekretariat'; break;
+			case('Fachlehrpersonen'):
+			case('Lehrpersonen'):
+			case('Fachpersonen'): $sPageName = 'lehrpersonen'; break;
+			case('Schulleitung'): $sPageName = 'schulleitung'; break;
+			case('Hauswartung'): $sPageName = 'hauswartung'; break;
+			default: $sPageName = 'ueber-uns';
+		}
+		// Redirect any teaching person to their profile
+		if($oTeamMember && $sPageName === 'lehrpersonen') {
+			$oTargetPage = PageQuery::create()->filterByPageType('team_member')->findOne();
+			if($oTargetPage) {
+				self::redirectToTarget($oTeamMember->getLink($oTargetPage));
+			}
+		}
+		// Redirect main functions to the related page
+		if($oTeamMember && $sPageName !== null) {
+			$oTargetPage = PageQuery::create()->filterByNameOrIdentifier($sPageName)->findOne();
+			if($oTargetPage) {
+				self::redirectToTarget($oTargetPage);
+			}
+		}
+		// Redirect to home page
+		$oRootPage = PageQuery::create()->findRoot();
+		self::redirectToTarget($oRootPage->getLink());
+	}
+
+	private static function redirectToTarget($aParams) {
+		LinkUtil::redirectToManager($aParams, 'FrontendManager');
 	}
 }
