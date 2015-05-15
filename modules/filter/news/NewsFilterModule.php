f681de138b96d8b60463b0b10066769bd013d10c
Jürg Messmer <jm@mosaics.ch>
2015-05-15 15:58:41 +0200

    add navigation item cache check

--- modules/filter/news/NewsFilterModule.php
+++ modules/filter/news/NewsFilterModule.php
@@ -1,12 +1,24 @@
 <?php
 class NewsFilterModule extends FilterModule {
 	public function onNavigationItemChildrenRequested($oNavigationItem) {
-		if($oNavigationItem instanceof PageNavigationItem && $oNavigationItem->getMe()->getPageType() === 'news') {
-			$oPageType = PageTypeModule::getModuleInstance($oNavigationItem->getMe()->getPageType(), $oNavigationItem->getMe(), $oNavigationItem);
-			foreach($oPageType->listQuery()->select(array('Id', 'Headline'))->find() as $aData) {
-				$oNavigationItem->addChild(NewsNavigationItem::create($aData['Id'], $aData['Headline']));
-			}
+		if(!($oNavigationItem instanceof PageNavigationItem && $oNavigationItem->getType() === 'news')) {
+			return;
 		}
+		$oPageType = PageTypeModule::getModuleInstance($oNavigationItem->getMe()->getPageType(), $oNavigationItem->getMe(), $oNavigationItem);
+		foreach($oPageType->listQuery()->select(array('Id', 'Headline'))->find() as $aData) {
+			$oNavigationItem->addChild(NewsNavigationItem::create($aData['Id'], $aData['Headline']));
+		}
+	}
+
+	public function onNavigationItemChildrenCacheDetectOutdated($oNavigationItem, $oCache, $aContainer) {
+		if(!($oNavigationItem instanceof PageNavigationItem && $oNavigationItem->getType() === 'services')) {
+			return;
+		}
+		$bIsOutdated = &$aContainer[0];
+		if($bIsOutdated) {
+			return;
+		}
+		$bIsOutdated = $oCache->isOlderThan($oPageType->listQuery());
 	}
 
 	public function onPageHasBeenSet($oCurrentPage, $bIsNotFound, $oNavigationItem) {
