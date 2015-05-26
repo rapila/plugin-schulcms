fcb7508554ba63060ce5c02ec5298a9c402859ff
Jürg Messmer <jm@mosaics.ch>
2015-05-26 17:29:03 +0200

    add year to class link title (mainly for testing…)

--- modules/page_type/classes/output/ClassListOutput.php
+++ modules/page_type/classes/output/ClassListOutput.php
@@ -55,7 +55,7 @@ class ClassListOutput extends ClassOutput {
 		$oItemTemplate->replaceIdentifier('class_type_key', StringUtil::normalizeToASCII($oClass->getClassType()));
 		$oItemTemplate->replaceIdentifier('year', $oClass->getYearPeriod());
 		$oItemTemplate->replaceIdentifier('detail_link', LinkUtil::link($oClass->getLink($this->oPage)));
-		$oItemTemplate->replaceIdentifier('detail_link_title', StringPeer::getString('class.view_detail').' '.$oClass->getUnitName());
+		$oItemTemplate->replaceIdentifier('detail_link_title', StringPeer::getString('class.view_detail').' '.$oClass->getUnitName(). ' '.$oClass->getYear());
 		$oItemTemplate->replaceIdentifier('count_students', $oClass->countStudentsByUnitName());
 
 		// get related class teacher links, in case there are 3 class teachers, to be just ;=)
