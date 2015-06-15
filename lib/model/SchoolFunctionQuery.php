5657aae1fa9fa09990dcf7f06d96c3be8df45849
Jürg Messmer <jm@mosaics.ch>
2015-06-15 17:22:15 +0200

    fix method that never worked before

--- lib/model/SchoolFunctionQuery.php
+++ lib/model/SchoolFunctionQuery.php
@@ -6,13 +6,7 @@
 class SchoolFunctionQuery extends BaseSchoolFunctionQuery {
 
 	public function filterByNonTeachingFunctionGroups() {
-		return $this->filterByFunctionGroupIdAndSchoolId(SchoolPeer::getActiveFunctionGroupIds('team_members_others'));
-	}
-
-	public function filterByFunctionGroupIdAndSchoolId($mFunctionGroupIds, $iSchoolId = null) {
-		$this->filterByFunctionGroupId($mFunctionGroupIds);
-		$this->filterBySchoolId($iSchoolId === null ? SchoolPeer::getSchoolId() : $iSchoolId);
-		$this->joinFunctionGroup()->joinTeamMemberFunction();
+		$this->filterByFunctionGroupId(SchoolPeer::getActiveFunctionGroupIds('team_members_others'))->joinFunctionGroup()->joinTeamMemberFunction();
 		return $this;
 	}
 
