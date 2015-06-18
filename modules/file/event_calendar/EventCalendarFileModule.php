219065287ca5daeaa2475156681755f28a1a7613
Raphael Schweikert <any@sabberworm.com>
2015-06-19 01:10:00 +0200

    Add ability for class events

--- modules/file/event_calendar/EventCalendarFileModule.php
+++ modules/file/event_calendar/EventCalendarFileModule.php
@@ -6,19 +6,26 @@ class EventCalendarFileModule extends FileModule {
 	private $oQuery = null;
 	private $oDateQuery = null;
 	private $iClassId = null;
+	private $oClass = null;
 
-	public function __construct($aRequestPath = null, $iClassId = null) {
+	public function __construct($aRequestPath = null, $oClass = null) {
 		parent::__construct($aRequestPath);
-		$this->iClassId = $iClassId;
-		$this->oQuery = FrontendEventQuery::create()->filterByIsActive(true)->excludeClassEvents();
-		if($iClassId !== null) {
-			$this->oQuery->_or()->filterByClassId($iClassId);
+		$this->oQuery = FrontendEventQuery::create();
+		if(isset($_REQUEST['class'])) {
+			$oClass = SchoolClassQuery::create()->findPk($_REQUEST['class']);
+		}
+		if($oClass) {
+			$this->oClass = $oClass;
+			$this->oQuery->displayedInClass($oClass, isset($_REQUEST['local']));
+		} else {
+			$this->oQuery->excludeClassEvents(isset($_REQUEST['global']));
 		}
 		$oAYearAgo = new DateTime();
 		$oAYearAgo->sub(new DateInterval('P1Y'));
 		$this->oQuery->filterByDateEnd($oAYearAgo, Criteria::GREATER_THAN);
-		if(class_exists('SchoolDate')) {
+		if(class_exists('SchoolDate') && !isset($_REQUEST['local'])) {
 			$this->oDateQuery = SchoolDateQuery::create();
+			$this->oDateQuery->filterByDateEnd($oAYearAgo, Criteria::GREATER_THAN);
 		}
 		header("Content-Type: application/calendar; charset=".Settings::getSetting('encoding', 'db', 'utf-8'));
 	}
@@ -43,6 +50,11 @@ class EventCalendarFileModule extends FileModule {
 
 	private function getFeed() {
 		$oCalendar = new \Eluceo\iCal\Component\Calendar(Settings::getSetting('domain_holder', 'domain', 'example.com'));
+		if($this->oClass) {
+			$oCalendar->setName($this->oClass->getClassName());
+		} else {
+			$oCalendar->setName(SchoolSiteQuery::currentSchoolSite()->getName());
+		}
 		foreach($this->oQuery->find() as $oEvent) {
 			$oCalendarEvent = new \Eluceo\iCal\Component\Event();
 			$oCalendarEvent
