92516beb4ed626534278dbd0625c91399289ea7b
Jürg Messmer <jm@mosaics.ch>
2015-09-15 17:36:37 +0200

    render all possible news navigation items
    … not only the ones configured in the news page (news_page_type)
    also news that are not explicitly displayed in common news list should be displayed. Example: The Elternteam list is only display at the respective page and not again on the common page, but the detail should be accessible never the less

--- modules/filter/news/NewsFilterModule.php
+++ modules/filter/news/NewsFilterModule.php
@@ -4,10 +4,13 @@ class NewsFilterModule extends FilterModule {
 		if(!($oNavigationItem instanceof PageNavigationItem && $oNavigationItem->getType() === 'news')) {
 			return;
 		}
-		$oPageType = PageTypeModule::getModuleInstance($oNavigationItem->getMe()->getPageType(), $oNavigationItem->getMe(), $oNavigationItem);
-		foreach($oPageType->listQuery()->select(array('Id', 'Headline'))->find() as $aData) {
+		foreach(self::query()->select(array('Id', 'Headline'))->find() as $aData) {
 			$oNavigationItem->addChild(NewsNavigationItem::create($aData['Id'], $aData['Headline']));
 		}
+	}
+
+	public static function query() {
+		return FrontendNewsQuery::create()->current()->excludeExternallyManaged();
 	}
 
 	public function onNavigationItemChildrenCacheDetectOutdated($oNavigationItem, $oCache, $aContainer) {
@@ -18,8 +21,7 @@ class NewsFilterModule extends FilterModule {
 		if($bIsOutdated) {
 			return;
 		}
-		$oPageType = PageTypeModule::getModuleInstance($oNavigationItem->getMe()->getPageType(), $oNavigationItem->getMe(), $oNavigationItem);
-		$bIsOutdated = $oCache->isOlderThan($oPageType->listQuery());
+		$bIsOutdated = $oCache->isOlderThan(self::query());
 	}
 
 	public function onPageHasBeenSet($oCurrentPage, $bIsNotFound, $oNavigationItem) {
