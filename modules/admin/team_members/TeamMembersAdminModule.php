ed863c337c8c7bf19c5e66ea9ababdda45196c20
Thomas Gressly <founder@foundit.ch>
2017-01-03 00:16:15 +0100

    Fix StringPeer for PHP 7

--- modules/admin/team_members/TeamMembersAdminModule.php
+++ modules/admin/team_members/TeamMembersAdminModule.php
@@ -37,7 +37,7 @@ class TeamMembersAdminModule extends AdminModule {
 				$aResult['field_name'] = 'id';
 				break;
 			case 'name':
-				$aResult['heading'] = StringPeer::getString('wns.team_members.sidebar_heading');
+				$aResult['heading'] = TranslationPeer::getString('wns.team_members.sidebar_heading');
 				break;
 			case 'magic_column':
 				$aResult['display_type'] = ListWidgetModule::DISPLAY_TYPE_CLASSNAME;
@@ -50,7 +50,7 @@ class TeamMembersAdminModule extends AdminModule {
 	public function getCustomListElements() {
 		return array(
 			array('function_group_id' => CriteriaListWidgetDelegate::SELECT_ALL,
-						'name' => StringPeer::getString('wns.sidebar.select_all'),
+						'name' => TranslationPeer::getString('wns.sidebar.select_all'),
 						'magic_column' => 'all')
 						);
 	}
